90f387fcabbc428f727e68dc3af7ff2f722ca154
Yuganshu Mahajan <yuganshu.mahajan@backcountry.com>
2023-02-02 17:04:09 +0530

    FIRST COMMIT ON BLOG WEBSITE PROJECT

---
+++ src/main/java/com/blogApp/BlogAppApplication.java
@@ -0,0 +1,15 @@
+package com.blogApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BlogAppApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApplication.class, args);
+	}
+
+}
